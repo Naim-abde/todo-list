001d1133b4cca14fd71fc280b0e3671c28651dfa
unknown <naimabdessamad48@gmail.com>
2025-03-21 19:57:14 +0000

    create delite read

--- script/main.js
+++ script/main.js
@@ -20,19 +20,21 @@ var lists = [
  function turnlist(){
     document.getElementById('cards').innerHTML = ""
 
- 
+      let num = 0
      for(list of lists) {
+         
         document.getElementById("cards").innerHTML += `<div class="card"> 
                         <div class="write"> 
-                            <h3 id="name">${list.name}</h3>
-                            <i class="fa-regular fa-calendar-days" style="margin-top: 5px;font-size: small; margin-right: 5px;"></i><span id="time">${list.time}</span>
+                            <h3 id="name"  >${list.name}</h3>
+                            <i class="fa-regular fa-calendar-days"   style="margin-top: 5px;font-size: small; margin-right: 5px;"></i><span id="time">${list.time}</span>
                         </div>
                         <div class="icon">
-                            <i class="fa-solid fa-trash" style="background-color: red; color: white;"></i>           
+                            <i class="fa-solid fa-trash" onclick="delite(${num})" style="background-color: red; color: white;"></i>           
                             <i class="fa-solid fa-check" style="background-color: greenyellow; color: white;"></i>
                             <i class="fa-solid fa-pen" style="background-color: rgb(47, 127, 255); color: white;"></i>
                         </div>
                     </div>`
+                    num++
     
     }            
      
@@ -41,14 +43,30 @@ var lists = [
 turnlist()  
 document.getElementById("plus").addEventListener("click",function(){
     var  text = prompt("enter text")
+    let now = new Date();
+    let day = now.getDate();  
+    let month = now.getMonth() + 1;  
+    let year = now.getFullYear();  
+    let date = day +"/"+  month  +"/"+year      
+
     var newTask = {
         "name":text,
-        "time":"21/15/2002",
+        "time":date,
         "done":false,
     }
     lists.push(newTask)
     turnlist()
+
 })
+function delite(num){
+    if(confirm("yes or no")){
+        lists.splice(num,1)
+        turnlist()
+    }
+    
+    
+ }
+ 
  
 
  
